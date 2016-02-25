ed2a1abec2cadd34e96e7d5810379982842ea3fe
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-02-25 14:59:01 -0500

    Finish basic design tweaks

--- wp-content/themes/freak/footer.php
+++ wp-content/themes/freak/footer.php
@@ -14,12 +14,12 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info container">
-			<?php printf( __( 'Theme Designed by %1$s.', 'freak' ), '<a href="'.esc_url("http://rohitink.com").'" rel="nofollow">Rohit Tripathi</a>' ); ?>
+			<?php // printf( __( 'Theme Designed by %1$s.', 'freak' ), '<a href="'.esc_url("http://rohitink.com").'" rel="nofollow">Rohit Tripathi</a>' ); ?>
 			<span class="sep"></span>
 			<?php echo ( get_theme_mod('freak_footer_text') == '' ) ? ('&copy; '.date('Y').' '.get_bloginfo('name').__('. All Rights Reserved. ','freak') ) : esc_html( get_theme_mod('freak_footer_text') ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
-	
+
 </div><!-- #page -->
 
 
--- wp-content/themes/freak/framework/layouts/content-freak.php
+++ wp-content/themes/freak/framework/layouts/content-freak.php
@@ -7,7 +7,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('col-md-12 col-sm-12 grid freak'); ?>>
 
 		<div class="featured-thumb col-md-5 col-sm-4">
-			<?php if (has_post_thumbnail()) : ?>	
+			<?php if (has_post_thumbnail()) : ?>
 				<a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><?php the_post_thumbnail('freak-thumb'); ?></a>
 			<?php else: ?>
 				<a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><img src="<?php echo get_template_directory_uri()."/assets/images/placeholder2.jpg"; ?>"></a>
@@ -15,7 +15,7 @@
 			<div class="postedon">
 				<div class="date"><?php echo get_the_date('M j'); ?></div>
 				<div class="author"><?php the_author_posts_link() ?></div>
-			</div>	
+			</div>
 		</div><!--.featured-thumb-->
 			
 		<div class="out-thumb col-md-7 col-sm-8">
@@ -25,7 +25,7 @@
 				<span class="readmore"><a class="hvr-underline-from-center" href="<?php the_permalink() ?>"><?php _e('Read More','freak'); ?></a></span>
 			</header><!-- .entry-header -->
 		</div><!--.out-thumb-->
-			
-		
-		
+
+
+
 </article><!-- #post-## -->

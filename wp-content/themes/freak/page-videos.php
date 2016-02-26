12554ddb812efb29614c86a3e402962a9ac85949
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-02-26 17:30:12 -0500

    Get rid of extra search bar

---
+++ wp-content/themes/freak/page-videos.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Videos
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package freak
+ */
+
+get_header(); ?>
+
+	<div id="primary-mono" class="content-area <?php do_action('freak_primary-width') ?> page">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+        $query = new WP_Query('cat=3');
+        while ( $query->have_posts() ) : $query->the_post();
+      ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+        <?= var_dump(get_author_posts_url()) ?>
+
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>

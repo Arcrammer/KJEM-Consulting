dec655332beaba67bc3450b60916119969ae0873
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-03-03 20:58:23 -0500

    Show Press Releases at /press

--- wp-content/themes/freak/page-press-releases.php
+++ wp-content/themes/freak/page-press-releases.php
@@ -1,35 +1,45 @@
 <?php
 /**
- * Template Name: Press Releases
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package freak
- */
+  * Template Name: Press Releases
+  *
+  * This is the template that displays all pages by default.
+  * Please note that this is the WordPress construct of pages
+  * and that other 'pages' on your WordPress site will use a
+  * different template.
+  *
+  * @package freak
+  */
 
-get_header(); ?>
+  get_header();
+?>
 
-	<div id="primary-mono" class="content-area <?php do_action('freak_primary-width') ?> page">
-		<main id="main" class="site-main" role="main">
+<div id="primary-mono" class="content-area <?php do_action('freak_primary-width') ?> page">
+  <main id="main" class="site-main featured-videos" role="main">
+    <h1 class="page-description">Press releases and media resources.</h1>
+    <h4 class="page-description">If you’re looking for the latest events at KJEM, you’re at the right place! For enquiries contact Karen Jones at <em>(510) 493 - 3326</em> or send mail to <em>mediasupport@kjemconsulting.com</em>.</h4>
+    <hr />
+    <?php
+      $query = new WP_Query('post_type=press-releases');
 
-			<?php while ( have_posts() ) : the_post(); ?>
+      while ($query->have_posts()):
+    ?>
+      <article class="press-release">
+        <?php $query->the_post() ?>
+        <p><a href="<?= get_the_permalink() ?>"><?= get_the_author() ?></a> posted on <?= get_the_date() ?></p>
+        <h4><?= get_the_title() ?></h4>
+        <?= get_the_content() ?>
+        <a href="<?= get_the_permalink() ?>">Read More...</a>
+      </article>
+    <?php
+      endwhile;
 
-				<?php get_template_part( 'content', 'page' ); ?>
+      // If comments are open or we have at least one comment, load up the comment template
+      if (comments_open() || get_comments_number()) {
+        comments_template();
+      }
+    ?>
+  </main> <!-- #main -->
+</div> <!-- #primary -->
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_sidebar() ?>
+<?php get_footer() ?>

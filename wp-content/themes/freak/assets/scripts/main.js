96a4596466c997cbef26607a3211fc2f31e25162
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-03-05 10:11:44 -0500

    Fix banner dismissal bug

--- wp-content/themes/freak/assets/scripts/main.js
+++ wp-content/themes/freak/assets/scripts/main.js
@@ -1,9 +1,21 @@
 (function ($) {
-  $('.close-banner').click(function (event) {
-    // Hide the banner
+  // Announcement banner stuff
+  var banner = $('.banner');
+  var headerTitle = $('#masthead.header-medium .site-branding');
+
+  // Move the header text down, consequently
+  // pushing the rest of the content down
+  headerTitle.css({
+    'padding-top': parseInt(headerTitle.css('padding-top')) + banner.height()
+  });
+
+  // Hide the banner when the cross is clicked
+  banner.click(function (event) {
     $(event.target).closest('.banner').css('display', 'none');
 
-    // Move the header image up
-    $('.site-header').css('background-position', '50% 0');
+    // Move the content up again
+    headerTitle.css({
+      'padding-top': parseInt(headerTitle.css('padding-top')) - banner.height()
+    });
   });
 })(jQuery);

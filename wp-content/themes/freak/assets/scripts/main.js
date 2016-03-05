6d844319ad205cbf34df724268faf85cf1fc51a5
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-03-05 12:06:16 -0500

    Compensate banner position if #wpadminbar is showing

--- wp-content/themes/freak/assets/scripts/main.js
+++ wp-content/themes/freak/assets/scripts/main.js
@@ -1,24 +1,32 @@
 (function ($) {
-  // Announcement banner stuff
-  var banner = $('.banner');
-  var headerTitle = $('#masthead.header-medium .site-branding');
+  $(document).ready(function () {
+    // Announcement banner stuff
+    var banner = $('.banner');
+    var headerTitle = $('#masthead.header-medium .site-branding');
+    var adminBar = $('#wpadminbar');
+    var extraHeight = (adminBar.length) ? + adminBar.height() + banner.height() : banner.height();
 
-  // Move the header text down, consequently
-  // pushing the rest of the content down
-  headerTitle.css({
-    'padding-top': parseInt(headerTitle.css('padding-top')) + banner.height()
-  });
-
-  // Hide the banner when the cross is clicked
-  banner.click(function (event) {
-    $(event.target).closest('.banner').css('display', 'none');
-
-    // Move the content up again
+    // Move the header text down, consequently
+    // pushing the rest of the content down
     headerTitle.css({
-      'padding-top': parseInt(headerTitle.css('padding-top')) - banner.height()
+      'padding-top': parseInt(headerTitle.css('padding-top')) + extraHeight
     });
 
-    // Tell the server to stop rendering it
-    $.ajax({url: '/?action=set_prefers_no_banner'});
+    banner.css({
+      'top': adminBar.height()
+    });
+
+    // Hide the banner when the cross is clicked
+    banner.click(function (event) {
+      $(event.target).closest('.banner').css('display', 'none');
+
+      // Move the content up again
+      headerTitle.css({
+        'padding-top': parseInt(headerTitle.css('padding-top')) - extraHeight
+      });
+
+      // Tell the server to stop rendering it
+      $.ajax({url: '/?action=set_prefers_no_banner'});
+    });
   });
 })(jQuery);

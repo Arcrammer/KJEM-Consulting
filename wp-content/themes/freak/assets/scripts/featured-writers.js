9cd22d867ac4566d9b15a0a5f90bad921548342c
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-03-09 07:39:54 -0500

    Adapt for featured audio

--- wp-content/themes/freak/assets/scripts/featured-writers.js
+++ wp-content/themes/freak/assets/scripts/featured-writers.js
@@ -2,8 +2,27 @@
   $('video').mediaelementplayer({
     videoWidth: '100%',
     videoHeight: '75%',
-    featured: [],
+    features: [],
     success: function (me) {
+      me.play();
+    }
+  });
+
+  $('audio').mediaelementplayer({
+    audioWidth: '100%',
+    audioHeight: '75%',
+    features: [
+      'playpause',
+      'progress',
+      'current',
+      'duration',
+      'tracks',
+      'volume',
+      'fullscreen'
+    ],
+    success: function (me) {
+      // Show the controls >_>
+      $('.mejs-container .mejs-controls').css('display', 'block');
       me.play();
     }
   });
--- wp-content/themes/freak/single-featured-video.php
+++ wp-content/themes/freak/single-featured-video.php
@@ -48,22 +48,31 @@ get_header(); ?>
   <div class="container content">
     <div id="primary-mono" class="content-area <?php do_action('freak_primary-width') ?>">
       <main id="main" class="site-main" role="main">
-        <?php while (have_posts()): the_post(); ?>
-          <video width="320" height="240" poster="<?= $video['url_without_ext'] ?>.jpg" controls="controls" preload="none">
-            <!-- MP4 for Safari, IE9, iPhone, iPad, Android, and Windows Phone 7 -->
-            <source type="video/mp4" src="<?= $video['url_without_ext'] ?>.mp4" />
-            <!-- WebM/VP8 for Firefox 4, Opera, and Chrome -->
-            <source type="video/webm" src="<?= $video['url_without_ext'] ?>.webm" />
-            <!-- Ogg/Vorbis for older Firefox and Opera versions -->
-            <source type="video/ogg" src="<?= $video['url_without_ext'] ?>.ogv" />
-            <!-- Flash fallback for non-HTML5 browsers without JavaScript -->
-            <object width="320" height="240" type="application/x-shockwave-flash" data="flashmediaelement.swf">
-              <param name="movie" value="flashmediaelement.swf" />
-              <param name="flashvars" value="controls=true&file=<?= $video['url_without_ext'] ?>.mp4" />
-              <!-- Image as a last resort -->
-              <img src="<?= $video['url_without_ext'] ?>.jpg" width="320" height="240" title="No video playback capabilities" />
-            </object>
-          </video>
+        <?php
+          while (have_posts()):
+            the_post();
+
+            $is_video = preg_match('/video/', get_field('file')['mime_type']);
+
+            if ($is_video): ?>
+              <video width="320" height="240" poster="<?= $video['url_without_ext'] ?>.jpg" controls="controls" preload="none">
+                <!-- MP4 for Safari, IE9, iPhone, iPad, Android, and Windows Phone 7 -->
+                <source type="video/mp4" src="<?= $video['url_without_ext'] ?>.mp4" />
+                <!-- WebM/VP8 for Firefox 4, Opera, and Chrome -->
+                <source type="video/webm" src="<?= $video['url_without_ext'] ?>.webm" />
+                <!-- Ogg/Vorbis for older Firefox and Opera versions -->
+                <source type="video/ogg" src="<?= $video['url_without_ext'] ?>.ogv" />
+                <!-- Flash fallback for non-HTML5 browsers without JavaScript -->
+                <object width="320" height="240" type="application/x-shockwave-flash" data="flashmediaelement.swf">
+                  <param name="movie" value="flashmediaelement.swf" />
+                  <param name="flashvars" value="controls=true&file=<?= $video['url_without_ext'] ?>.mp4" />
+                  <!-- Image as a last resort -->
+                  <img src="<?= $video['url_without_ext'] ?>.jpg" width="320" height="240" title="No video playback capabilities" />
+                </object>
+              </video>
+            <?php else: ?>
+              <audio src="<?= $video['url'] ?>" type="<?= $video['mime_type'] ?>"></audio>
+            <?php endif ?>
         <?php
           // If comments are open or we have at least one comment, load the comment template
           if (comments_open() || get_comments_number()) comments_template();
